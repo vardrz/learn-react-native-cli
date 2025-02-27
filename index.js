d79858312857dd5eb40a2d2951e51a3e247a8fbe
vardrz <fatkhurrozakf@gmail.com>
2025-02-27 11:06:27 +0700

    feat: fcm global notif

--- index.js
+++ index.js
@@ -5,5 +5,17 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import { getMessaging, onMessage, setBackgroundMessageHandler } from '@react-native-firebase/messaging';
+import { getApp } from '@react-native-firebase/app';
+
+const messaging = getMessaging(getApp());
+
+setBackgroundMessageHandler(messaging, async (remoteMessage) => {
+    console.log("Background Notification!", JSON.stringify(remoteMessage));
+});
+
+onMessage(messaging, async (remoteMessage) => {
+    console.log("New Notification!", JSON.stringify(remoteMessage));
+});
 
 AppRegistry.registerComponent(appName, () => App);
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,52 @@
 import AppNavigator from "./navigation/AppNavigator";
 import { AuthProvider } from "./contexts/AuthContext";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useEffect } from 'react';
+import { getMessaging, getToken, requestPermission, subscribeToTopic } from "@react-native-firebase/messaging";
+import { getApp } from "@react-native-firebase/app";
+import { PermissionsAndroid, Platform } from "react-native";
 
 export default function Index() {
+  useEffect(() => {
+    const requestUserPermission = async () => {
+      try {
+        const messaging = getMessaging(getApp());
+        const authStatus = await requestPermission(messaging);
+
+        console.log("Notif Permission Status : ", authStatus);
+      } catch (error) {
+        console.error("Error requesting permission:", error);
+      }
+    };
+
+    const subsGlobalTopic = async () => {
+      try {
+        const messaging = getMessaging(getApp());
+        await subscribeToTopic(messaging, "global")
+        console.log("Subs Topic : global");
+      } catch (error) {
+        console.error("Error subs topic : global");
+      }
+    };
+
+    requestAndroidNotificationPermission();
+    requestUserPermission();
+    subsGlobalTopic();
+  }, []);
+
+  const requestAndroidNotificationPermission = async () => {
+    if (Platform.OS === "android" && Platform.Version >= 33) {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("Android 13 Notification permission granted!");
+      } else {
+        console.log("Android 13 Notification permission denied!");
+      }
+    }
+  };
+
   return (
     <SafeAreaProvider>
       <AuthProvider>
